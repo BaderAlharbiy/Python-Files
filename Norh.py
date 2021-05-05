6e63cc7f7e17ed4905b9221ec53e538208a976d3
BaderAlharbiy <cool-2000@hotmail.com>
2021-05-05 03:25:20 +0300

    2nd commenets

---
+++ Norh.py
@@ -0,0 +1,4 @@
+x=float(input("1No"))
+y=float(input("2No"))
+
+Print(2**3**3)
---
+++ class_test.py
@@ -0,0 +1,115 @@
+class Location:
+	def __init__(self,name,):
+		self.name=name
+		self.country=country
+
+lo = Location("Sami","ksa")
+
+
+class mother(object):
+	def eye_color(self):
+		print("blue")
+	def suger(self):
+		print("suger")
+class father(object):
+	def high(self):
+		print("high")
+	def nose(self):
+		print("nose")
+
+class child(mother,father):
+	def paly(self):
+		print("playing")
+
+
+ahmed=child()
+print(ahmed.high())
+print(ahmed.eye_color())
+#class myinfo():
+#	def __init__(self,name,age,address,city):
+#		self.name=name
+#		self.age=age
+#		self.address=address
+#		self.city=city
+#
+#	def myName(self):
+#		print("My name is:",self.name)
+#	def myAge(self):
+#		print("My age is:",self.age)
+#	def myAddress(self):
+#		print("My address is:",self.address)
+#	def myCity(self):
+#		print("My city is:",self.city)
+
+#class infomation():
+#	def __init__(self):
+#		self.name=name
+		
+
+#	def myName(self):
+#		print("My name is:",self.name)
+#	def myAge(self):
+#		print("My age is:",self.age)
+#	def myAddress(self):
+#		print("My address is:",self.address)
+#	def myCity(self):
+#		print("My city is:",self.city)
+	#def eat(self):
+	#	print("eating !!!!!!")
+
+#bader=infomation("salm")
+#bader = info("bader",22,"K.S.A","riyadh")
+#print(bader.eat())
+
+
+#print(bader.age)
+#bader.myAddress()
+
+
+
+
+
+#bader = myinfo("bader",22,"K.S.A","riyadh")
+#print(bader.age)
+#print(bader.myAge())
+#print(bader.myCity())
+#bader.myAge()
+#print(bader.name)
+#print(bader.age)
+#print(bader.address)
+#print(bader.city)
+
+
+ 
+
+
+
+
+
+
+#class Bader(object):
+#	def __init__(self,name):
+#		self.name=name
+#	def say_hi(self):
+#		print("Hello My name is mr",self.name,"is sami")
+
+
+#class bader1(Bader):
+#		#self.address=address
+#	def paly(self):
+#		return "test !!!!!!!"
+		
+
+
+#b1 = bader1("SAMI")
+#print(b1.name)
+#print(b1.say_hi())
+#bader = Bader("Bader")
+#bader.say_hi()
+#sbader = Bader1("SOSOSO")
+
+#mazer = Bader("Bader")
+#mazer.test()
+
+#print(mazer.name) 
+#print(mazer.say_hi())
