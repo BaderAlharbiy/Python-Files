ec8901497ffe253f3aa9dd11136822f59c0d31ca
BaderAlharbiy <cool-2000@hotmail.com>
2021-05-05 23:08:57 +0300

    This file is last edit

---
+++ testCopy.py
@@ -0,0 +1,23 @@
+#import hashlib
+
+c=open(ss.txt)
+#d=c.read
+#print(d)
+
+#file = input("Enter file name; ")
+#with open(file,mode='r')as f:
+#	for line in f:
+#		print(line)
+
+#os.remove("bader.txt")
+#shutil.copy("ACC/bader.txt","Desktop")
+#help("hashlib")
+#print(hashlib.md5("Bader".encode()).hexdigest())
+
+#word=input("please enter your word to encrypt: ")
+#print(word)
+#md5 = hashlib.md5("word".encode()).hexdigest()
+#print(md5)
+
+#sha224 = hashlib.sha512("test".encode()).hexdigest()
+#print(sha224)
